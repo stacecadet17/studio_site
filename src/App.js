8a0b4175403e8a004a7250319cea717a2c861b71
stacecadet17 <staceyabbott@stacey-abbotts-macbook.local>
2020-09-13 18:32:05 -0700

    added about container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./components/nav/NavBar";
 import Slider from "./components/carousel/Slider";
+import Video from "./components/video/Video";
 import "./custom.scss";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Slider />
+      <Video />
     </div>
   );
 }
---
+++ src/components/video/Video.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+class Video extends React.Component {
+  render() {
+    return (
+      <div>
+        <Container fluid className="bg-primary">
+          <Row>
+            <Col>
+              <Card>
+                <Card.Title>
+                  Welcome to the Nancy Doizé School Of Dance ﻿﻿﻿​﻿Conveniently
+                  located at 4620 Meridian Avenue, San Jose California, The
+                  Nancy Doizé School of Dance has been serving the community
+                  with a first rate dance program for over 30 years. We offer
+                  dance classes in ballet, jazz, lyrical, hip hop, tap and
+                  modern, as well as fitness classes in pilates and Zumba. The
+                  studio has three separate classrooms, and teachers who are
+                  passionate about all disciplines of dance. We offer an amazing
+                  recreational dance program for ages three to adult, as well as
+                  a cutting edge performance and competitive dance program for
+                  the dancer ready to take it to the next level. The studio's
+                  Competition and Performance Team is appropriately titled the
+                  "Doizé Dream Team". The team has been National Champions for
+                  the past ten years.{" "}
+                </Card.Title>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <iframe
+                  width="100%"
+                  height="315"
+                  src="https://www.youtube.com/embed/pP7go-DTNg0"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                ></iframe>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Video;
